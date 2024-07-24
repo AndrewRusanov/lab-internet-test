8fef6cc50a29063216cd6e8c1966bdf515a0ce73
Andrew Rusanov <rusanow.andrej2018@yandex.ru>
2024-07-24 22:53:49 +0300

    feat: add adaptive header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import classNames from "classnames";
 
 import logo from "assets/images/logoHeader.svg";
+import logoMenu from "assets/images/logoMenu.svg";
+import burgerMenu from "assets/images/burgerMenu.svg";
+import crossBtn from "assets/images/crossBtn.svg";
+import arrowRightMenu from "assets/images/arrowRightMenu.svg";
 
 import { navBarList } from "data/constants";
 
@@ -9,32 +13,34 @@ import styles from "./Header.module.css";
 
 const Header = () => {
   const [headerScrolled, setHeaderScrolled] = useState(false);
-  const [isActive, setIsActive] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     const listener = () => {
-      if (window.scrollY >= 960) {
+      if (window.scrollY >= 10) {
         setHeaderScrolled(true);
       } else {
         setHeaderScrolled(false);
       }
     };
-
     window.addEventListener("scroll", listener);
-
     return () => {
       window.removeEventListener("scroll", listener);
     };
   }, []);
 
-  const handleTabClick = () => {
-    setIsActive((prev) => !prev);
-  };
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.style.overflowY = "hidden";
+    }
+    return () => {
+      document.body.style.overflowY = "auto";
+    };
+  }, [isMenuOpen]);
 
-  const activeTabStyle = classNames({
-    [styles.menuItem]: !isActive,
-    [styles.menuItemActive]: isActive,
-  });
+  const handleMenuClick = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
 
   const headerStyle = classNames({
     [styles.header]: !headerScrolled,
@@ -44,15 +50,52 @@ const Header = () => {
   return (
     <header className={headerStyle}>
       <img src={logo} alt="Логотип проекта" className={styles.logo} />
-      <nav>
-        <ul className={styles.menu}>
+      <nav className={styles.desktopNavigation}>
+        <ul className={styles.navigationList}>
           {navBarList.map((item, index) => (
-            <li key={index} className={activeTabStyle} onClick={handleTabClick}>
+            <li key={index} className={styles.navigationItem}>
               {item.text}
             </li>
           ))}
         </ul>
       </nav>
+
+      <div className={styles.laptopNavigation}>
+        <button
+          type="button"
+          className={styles.burgerButton}
+          onClick={handleMenuClick}
+        >
+          <img src={burgerMenu} alt="Бургер-меню" />
+        </button>
+      </div>
+
+      {isMenuOpen && (
+        <nav className={styles.menu}>
+          <div className={styles.menuHead}>
+            <img
+              src={logoMenu}
+              alt="Логотип проекта"
+              className={styles.logoMenu}
+            />
+            <button
+              type="button"
+              className={styles.crossBtn}
+              onClick={handleMenuClick}
+            >
+              <img src={crossBtn} alt="Закрыть меню" />
+            </button>
+          </div>
+          <ul className={styles.menuList}>
+            {navBarList.map((item, index) => (
+              <li key={index} className={styles.menuItem}>
+                <span className={styles.menuItemText}>{item.text}</span>
+                <img src={arrowRightMenu} alt="Переход к разделу" />
+              </li>
+            ))}
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
