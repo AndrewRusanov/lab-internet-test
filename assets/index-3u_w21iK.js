121e7d898afced0360259050f6b1b0fdbe4d1f14
Andrew Rusanov <rusanow.andrej2018@yandex.ru>
2024-07-26 11:46:39 +0300

    Rebuild GitHub pages

---
+++ assets/index-3u_w21iK.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-y1lXOnZk.js";const e=()=>t.jsx("svg",{width:"16",height:"12",viewBox:"0 0 16 12",fill:"none",xmlns:"http://www.w3.org/2000/svg",children:t.jsx("path",{d:"M0 6.51044L5.4791 12L16 1.4895L14.4896 -6.29425e-05L5.4791 8.99993L1.48953 5.01039L0 6.51044Z",fill:"white"})}),h=e;export{h as default};
